f368b4d6bbed750a26e45a16d18ce22f142caee6
Alex <79322837+Topal-Alex@users.noreply.github.com>
2022-05-14 12:16:35 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Linq;
+
+
+namespace ConsoleApplication1
+{
+    class  Program
+    {
+        static void Main(string[] args)
+        {
+            
+
+        }
+        public static IEnumerable<int> BaumSweet()
+        {
+            for (int a = 0; a < 1000000; ++a)
+                yield return Check(a) ? 1 : 0;
+
+            static bool Check(int a)
+            {
+                while (a != 0)
+                {
+                    if ((a & 1) == 1)
+                        a >>= 1;
+                    else
+                    {
+                        if ((a & 3) == 0)
+                            a >>= 2;
+                        else
+                            return false;
+                    }
+                }
+                return true;
+            }
+        }
+
+    }
+
+}
